4722d26546a688fcfea22b04dd8a0d2e50dc6e87
Florian Rappl <florian.rappl@smapiot.com>
2024-06-28 08:48:32 +0200

    Update Reviews.tsx

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -27,7 +27,7 @@ function Reviews() {
         />
       </a>
       <CopyInfo>
-        Currently sold: <b>4,709</b> copies (31st of December 2023).
+        Currently sold: <b>4,709</b> copies (31st of March 2024).
       </CopyInfo>
     </Background>
   );
